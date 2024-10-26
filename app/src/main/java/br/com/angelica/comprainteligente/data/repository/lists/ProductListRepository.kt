4f1159b526854687214a8054bdba1f560ba8d114
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2024-10-26 00:11:55 -0300

    Reorganize classes

--- app/src/main/java/br/com/angelica/comprainteligente/data/repository/lists/ProductListRepository.kt
+++ app/src/main/java/br/com/angelica/comprainteligente/data/repository/lists/ProductListRepository.kt
@@ -5,10 +5,10 @@ import br.com.angelica.comprainteligente.model.ProductList
 import com.google.firebase.Timestamp
 
 interface ProductListRepository {
-    suspend fun fetchProductsByIds(productIds: List<String>): Result<List<Product>>
-    suspend fun fetchUserLists(includeProductIds: Boolean): Result<List<ProductList>>
     suspend fun createList(listName: String, productIds: List<String>): Result<Unit>
     suspend fun updateList(listId: String, name: String, productIds: List<String>, data: Timestamp): Result<Unit>
     suspend fun deleteList(listId: String): Result<Unit>
     suspend fun getSuggestions(query: String): Result<List<Product>>
+    suspend fun fetchProductsByIds(productIds: List<String>): Result<List<Product>>
+    suspend fun fetchUserLists(includeProductIds: Boolean): Result<List<ProductList>>
 }
--- app/src/main/java/br/com/angelica/comprainteligente/data/repository/lists/ProductListRepositoryImpl.kt
+++ app/src/main/java/br/com/angelica/comprainteligente/data/repository/lists/ProductListRepositoryImpl.kt
@@ -15,45 +15,6 @@ class ProductListRepositoryImpl(
     private val productListCollection = firestore.collection("product_lists")
     private val productCollection = firestore.collection("products")
 
-    override suspend fun fetchProductsByIds(productIds: List<String>): Result<List<Product>> {
-        return try {
-            if (productIds.isNotEmpty()) {
-                val querySnapshot = productCollection
-                    .whereIn(FieldPath.documentId(), productIds)
-                    .get()
-                    .await()
-
-                val products = querySnapshot.documents.mapNotNull { it.toObject(Product::class.java) }
-                Result.success(products)
-            } else {
-                Result.success(emptyList())
-            }
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
-    override suspend fun fetchUserLists(includeProductIds: Boolean): Result<List<ProductList>> {
-        return try {
-            val querySnapshot = productListCollection
-                .orderBy("data", Query.Direction.DESCENDING) // ordena pela data de inserção
-                .get()
-                .await()
-
-            val lists = querySnapshot.documents.map { document ->
-                ProductList(
-                    id = document.id,  // Aqui estamos usando o ID gerado pelo Firestore
-                    name = document.getString("name") ?: "",
-                    productIds = if (includeProductIds) { document.get("productIds") as? List<String> ?: emptyList() } else { emptyList() },
-                    data = document.getTimestamp("data") ?: Timestamp.now()
-                )
-            }
-            Result.success(lists)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
     override suspend fun createList(listName: String, productIds: List<String>): Result<Unit> {
         return try {
             val newListId = productListCollection.document().id
@@ -96,6 +57,45 @@ class ProductListRepositoryImpl(
                 .await()
             val products = querySnapshot.documents.mapNotNull { it.toObject(Product::class.java) }
             Result.success(products)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun fetchProductsByIds(productIds: List<String>): Result<List<Product>> {
+        return try {
+            if (productIds.isNotEmpty()) {
+                val querySnapshot = productCollection
+                    .whereIn(FieldPath.documentId(), productIds)
+                    .get()
+                    .await()
+
+                val products = querySnapshot.documents.mapNotNull { it.toObject(Product::class.java) }
+                Result.success(products)
+            } else {
+                Result.success(emptyList())
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun fetchUserLists(includeProductIds: Boolean): Result<List<ProductList>> {
+        return try {
+            val querySnapshot = productListCollection
+                .orderBy("data", Query.Direction.DESCENDING) // ordena pela data de inserção
+                .get()
+                .await()
+
+            val lists = querySnapshot.documents.map { document ->
+                ProductList(
+                    id = document.id,  // Aqui estamos usando o ID gerado pelo Firestore
+                    name = document.getString("name") ?: "",
+                    productIds = if (includeProductIds) { document.get("productIds") as? List<String> ?: emptyList() } else { emptyList() },
+                    data = document.getTimestamp("data") ?: Timestamp.now()
+                )
+            }
+            Result.success(lists)
         } catch (e: Exception) {
             Result.failure(e)
         }
